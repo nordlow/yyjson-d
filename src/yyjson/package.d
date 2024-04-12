a38d34bf09358b638bb100bdc5c5157d5916f5f7
Per Nordlöw <per.nordlow@gmail.com>
2024-04-12 07:26:38 +0200

    Update src/yyjson/package.d:

--- src/yyjson/package.d
+++ src/yyjson/package.d
@@ -36,20 +36,14 @@ Document parseJSON(in char[] data, in Options options) @trusted pure nothrow @no
 	assert(doc);
 }
 
-//import yyjson_c; // ImportC yyjson.c. Functions are overrided below.
+// import yyjson_c; // ImportC yyjson.c. Functions are overrided below.
 // Need these because ImportC doesn't support overriding qualifiers.
-
-
-// Copied from BLAKE3/c/blake3.h
 extern(C) private pure nothrow @nogc {
 import core.stdc.stdint : uint32_t;
-
 struct yyjson_doc;
 struct yyjson_alc;
-
 alias yyjson_read_flag = uint32_t;
 alias yyjson_read_code = uint32_t;
-
 struct yyjson_read_err {
     /** Error code, see `yyjson_read_code` for all possible values. */
     yyjson_read_code code;
@@ -58,7 +52,6 @@ struct yyjson_read_err {
     /** Error byte position for input data (0 if success). */
     size_t pos;
 }
-
 yyjson_doc *yyjson_read_opts(scope const(char)* dat,
                              size_t len,
                              yyjson_read_flag flg,
