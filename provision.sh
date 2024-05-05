cd6d6ec793f7398241884c64e2125587bfca4603
Per Nordlöw <per.nordlow@gmail.com>
2024-05-05 11:17:40 +0200

    *** empty log message ***

--- provision.sh
+++ provision.sh
@@ -8,6 +8,11 @@ TARGET_ROOT="yyjson-build"
 mkdir -p "${TARGET_ROOT}"
 pushd "${TARGET_ROOT}" > /dev/null
 
+if ! command -v cmake >/dev/null 2>&1; then
+    echo "Installing cmake ..."
+    sudo apt install cmake
+fi
+
 # ok to use "-march=native" because of no distribution
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}"
 make --quiet
