ebf480f7dcd8e4251ed614708bc5405c096cc42b
Per Nordlöw <per.nordlow@gmail.com>
2024-05-08 07:52:36 +0200

    Update provision.sh, yyjson:

--- provision.sh
+++ provision.sh
@@ -4,14 +4,26 @@ set -euo pipefail
 
 SOURCE_ROOT="yyjson"
 TARGET_ROOT="yyjson-build"
+tools=("cmake")
+
+install_apt_packages_of_executables() {
+	local packages=()
+	for command in "$@"; do
+		if ! command -v "$command" >/dev/null 2>&1; then
+			packages+=("$command")
+		fi
+	done
+	if [[ ${#packages[@]} -gt 0 ]]; then
+		package_list="${packages[@]}"
+		echo "Installing missing APT packages: $package_list ..."
+		sudo apt install "${packages[@]}"
+	fi
+}
 
 mkdir -p "${TARGET_ROOT}"
 pushd "${TARGET_ROOT}" > /dev/null
 
-if ! command -v cmake >/dev/null 2>&1; then
-    echo "Installing cmake ..."
-    sudo apt install cmake
-fi
+install_apt_packages_of_executables "${tools[@]}"
 
 # ok to use "-march=native" because of no distribution
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}"
