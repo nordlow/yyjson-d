9f799da5847cc68ea509c5845f51616b4ff5f9f6
Per Nordlöw <per.nordlow@gmail.com>
2024-10-24 08:58:45 +0200

    Better printing

--- benchmark.sh
+++ benchmark.sh
@@ -2,5 +2,13 @@
 
 set -euo pipefail
 
+echo
+echo "# Benchmarking ..."
+
+echo
+echo "## Benchmarking with DMD ..."
 dub -q test --compiler=dmd --build=benchmark-release
+
+echo
+echo "## Benchmarking with LDC ..."
 dub -q test --compiler=ldc2 --build=benchmark-release
--- test.sh
+++ test.sh
@@ -2,5 +2,13 @@
 
 set -euo pipefail
 
+echo
+echo "# Testing ..."
+
+echo
+echo "## Testing with DMD ..."
 dub -q test --compiler=dmd
+
+echo
+echo "## Testing with LDC ..."
 dub -q test --compiler=ldc2
