aa931568df8e217ee5c52f124b9409b37081c67c
Per Nordlöw <per.nordlow@gmail.com>
2024-10-24 09:02:13 +0200

    Modify benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -2,11 +2,10 @@
 
 set -euo pipefail
 
-echo -e "echo
-# Benchmarking ..."
+echo -e "\n# Benchmarking ..."
 
 echo -e "\n## Benchmarking with DMD ..."
 dub -q test --compiler=dmd --build=benchmark-release
 
-echo e "\n## Benchmarking with LDC ..."
+echo -e "\n## Benchmarking with LDC ..."
 dub -q test --compiler=ldc2 --build=benchmark-release
