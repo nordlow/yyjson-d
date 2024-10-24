0a031096be32228fc58ae9bc7de75248a7a30740
Per Nordlöw <per.nordlow@gmail.com>
2024-10-24 09:35:46 +0200

    Modify dub.sdl, test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo -e "\n# Testing ..."
 
 echo -e "\n## Testing with DMD ..."
-dub -q test --compiler=dmd
+dub -q test --compiler=dmd --build=unittest-without-coverage
 
 echo -e "\n## Testing with LDC using AddressSanitizer (ASan) ..."
-dub -q test --compiler=ldc2
+dub -q test --compiler=ldc2 --build=unittest-without-coverage
